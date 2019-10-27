dd999ba3e7e66ec67c30004f44443b221ed29eb7
Turar <turar0408@gmail.com>
2019-10-27 22:32:36 +0600

    New models created. Added connection between tables

--- mainapp/models.py
+++ mainapp/models.py
@@ -26,17 +26,27 @@ class Movie(models.Model):
     movie_findings = models.FloatField()
     #actors = models.
     #produccer
-    #genres
+    movie_genres = models.ManyToManyField(Movie)
     #comments
+
+
+class Genre(models.Model):
+    genre_id = models.CharField(max_length=9999)
+    genre_name = models.CharField(max_length=100)
 
 
 class Person(models.Model):
     person_id = models.CharField(max_length=9999)
     person_name = models.CharField(max_length=100)
-    #person_films = models.
     person_url_image = models.CharField(max_length=1000)
     person_rating_av_from_films = models.FloatField()
     #person_roles_in_film
+
+
+class Person_Movie(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
 
 
 class User(models.Model):
@@ -50,10 +60,18 @@ class User(models.Model):
     #ratings
 
 
+class Rating(models.Model):
+    rating_id = models.CharField(max_length=9999)
+    rating_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating_number = models.IntegerField()
+
+
 class Comment(models.Model):
     comment_id = models.CharField(max_length=9999)
     comment_description = models.TextField()
     comment_date = models.DateTimeField()
     #likes
     #dislikes
-    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    comment_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    comment_user = models.ForeignKey(Movie, on_delete=models.CASCADE)
