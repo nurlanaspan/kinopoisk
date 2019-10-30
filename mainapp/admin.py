d84e5e2be88c935c0a6ffa4eae89dfe26e3fc4dd
Turar <turar0408@gmail.com>
2019-10-30 17:57:50 +0600

    admin site modified

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Movie
+from .models import Person
+from .models import Genre
+from .models import User
+from .models import Person_Movie
+from .models import Rating
+from .models import Comment
 
 # Register your models here.
+admin.site.register(Person)
 admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(User)
+admin.site.register(Person_Movie)
+admin.site.register(Rating)
+admin.site.register(Comment)
